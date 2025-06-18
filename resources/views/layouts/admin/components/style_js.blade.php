9a9782aea795931ae0d12b8678a92786e2da222e
maulanaibrahim20 <maulibrahim20@student.polindra.ac.id>
2025-06-18 20:43:02 +0700

    stater pack init

--- resources/views/layouts/admin/components/style_js.blade.php
+++ resources/views/layouts/admin/components/style_js.blade.php
@@ -1,11 +1,3 @@
-<!-- Overlay -->
-<div class="layout-overlay layout-menu-toggle"></div>
-
-<!-- Drag Target Area To SlideIn Menu On Small Screens -->
-<div class="drag-target"></div>
-
-<!--/ Layout wrapper -->
-
 <!-- Core JS -->
 <!-- build:js assets/vendor/js/core.js -->
 <script src="{{ url('/admin') }}/vendor/libs/jquery/jquery.js"></script>
@@ -31,3 +23,33 @@
 
 <!-- Page JS -->
 <script src="{{ url('/admin') }}/js/dashboards-analytics.js"></script>
+
+<script>
+    $(document).on('click', '.open-global-modal', function(e) {
+        e.preventDefault();
+
+        const url = $(this).data('url');
+        const title = $(this).data('title') || 'Modal Title';
+
+        $('#globalModalTitle').text(title);
+        $('#globalModalBody').html('<div class="text-center">Loading...</div>');
+        $('#globalModalFooter').html(
+            '<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>');
+
+        $.ajax({
+            url: url,
+            type: 'GET',
+            success: function(response) {
+                $('#globalModalBody').html(response.body || response);
+                if (response.footer) {
+                    $('#globalModalFooter').html(response.footer);
+                }
+                $('#globalModal').modal('show');
+            },
+            error: function(xhr) {
+                $('#globalModalBody').html(
+                    '<div class="alert alert-danger">Failed to load content.</div>');
+            }
+        });
+    });
+</script>
--- resources/views/layouts/admin/main.blade.php
+++ resources/views/layouts/admin/main.blade.php
@@ -11,28 +11,26 @@
     <title>Dashboard - Analytics | Vuexy - Bootstrap Admin Template</title>
 
     @include('layouts.admin.components.style_css')
+    @stack('scripts')
 </head>
 
 <body>
     <!-- Layout wrapper -->
     <div class="layout-wrapper layout-navbar-full layout-horizontal layout-without-menu">
         <div class="layout-container">
-            <!-- Navbar -->
-
+            <!-- Header -->
             @include('layouts.admin.header')
-
-            <!-- / Navbar -->
+            <!-- / Header -->
 
             <!-- Layout container -->
             <div class="layout-page">
                 <!-- Content wrapper -->
                 <div class="content-wrapper">
-                    <!-- Menu -->
+                    <!-- Navbar -->
                     @include('layouts.admin.navbar')
-                    <!-- / Menu -->
+                    <!-- / Navbar -->
 
                     <!-- Content -->
-
                     <div class="container-fluid flex-grow-1 container-p-y">
                         @yield('content')
                     </div>
@@ -50,7 +48,33 @@
             <!--/ Layout container -->
         </div>
     </div>
+
+    <!-- Global Modal -->
+    <div class="modal fade" id="globalModal" data-bs-backdrop="static" tabindex="-1">
+        <div class="modal-dialog modal-dialog-centered modal-lg">
+            <form class="modal-content" id="globalModalForm">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="globalModalTitle">Loading...</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body" id="globalModalBody">
+                    <div class="text-center">Loading content...</div>
+                </div>
+                <div class="modal-footer" id="globalModalFooter">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+            </form>
+        </div>
+    </div>
+    <!-- Overlay -->
+    <div class="layout-overlay layout-menu-toggle"></div>
+
+    <!-- Drag Target Area To SlideIn Menu On Small Screens -->
+    <div class="drag-target"></div>
+
+    <!--/ Layout wrapper -->
     @include('layouts.admin.components.style_js')
+    @stack('scripts')
 </body>
 
 </html>
